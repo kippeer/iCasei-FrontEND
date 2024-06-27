1510c946b1d7242dc94a5f151236f8ea37d75111
Fabio <fabiot92@gmail.com>
2024-06-27 15:15:32 -0300

    att

--- bff/src/index.ts
+++ bff/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import youtubeRoutes from './routes/youtube';
 import favoritesRoutes from './routes/favorites';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
--- bff/src/routes/youtube.ts
+++ bff/src/routes/youtube.ts
@@ -1,8 +1,9 @@
+// No arquivo routes/youtube.ts
 import express, { Request, Response } from 'express';
 import axios from 'axios';
 
 const router = express.Router();
-const YOUTUBE_API_KEY = 'AIzaSyBJbe15qFM_mlbY6B3jwPjSE9Ond048mVc';
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
 router.get('/search', async (req: Request, res: Response) => {
   const query = req.query.q;
@@ -10,20 +11,17 @@ router.get('/search', async (req: Request, res: Response) => {
     return res.status(400).json({ error: 'Query parameter "q" must be a string' });
   }
 
-  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${YOUTUBE_API_KEY}`;
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(query)}&key=${YOUTUBE_API_KEY}`;
 
   try {
     const response = await axios.get(url);
     res.json(response.data);
   } catch (error: any) {
     if (error.response) {
-      // O servidor respondeu com um status de erro
       res.status(error.response.status).json({ error: error.message });
     } else if (error.request) {
-      // A requisição foi feita, mas não recebeu resposta
       res.status(500).json({ error: 'Erro de rede ao tentar acessar o servidor' });
     } else {
-      // Ocorreu um erro durante o processamento da requisição
       res.status(500).json({ error: 'Erro ao processar a requisição' });
     }
   }
